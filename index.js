805cc068a07e91a6a84d2db2180d53b8e2231e5c
Bez Hermoso <bezalelhermoso@gmail.com>
2016-06-27 11:33:36 -0700

    Code cleanup

--- index.js
+++ index.js
@@ -18,15 +18,12 @@ var action = argv._.length > 0 ? argv._[0] : null;
 
 var credentials = function () {
   return fs.readFileAsync(credentialsFile, 'utf8')
+    .then(JSON.parse)
     .catch(function () {
       return {
         username: process.env.ZENEFITS_USERNAME,
         password: process.env.ZENEFITS_PASSWORD,
       };
-    })
-    .then(JSON.parse)
-    .catch(function () {
-      return {};
     });
 }
 
@@ -50,7 +47,7 @@ credentials().then(function (creds) {
 
   if (!username || !password) {
     console.error('Can\'t find your Zenefits credentials.');
-    console.error('Either set the env variables ZENEFITS_USERNAME and ZENEFITS_PASSWORD in your shell, or save your credentials in ' + process.env.HOME + '/.zenefits.json as follows:');
+    console.error('Either set the env variables ZENEFITS_USERNAME and ZENEFITS_PASSWORD in your shell, or save your credentials in ' + credentialsFile + ' as follows:');
     console.error('');
     console.error(JSON.stringify({
       username: '<your username>',
